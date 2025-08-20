e73bc67e59c37f3ddc2a162f2bb48889bdb8b0c0
callsito <callsito.lgbt@gmail.com>
2025-08-21 00:07:28 +0200

    Le ficher tank-bqsic ajouté

---
+++ tank/tank-basic.ino
@@ -0,0 +1,78 @@
+#define ena = 10;
+#define in1 = 9;
+#define in2 = 8;
+#define in3 = 7;
+#define in4 = 6;
+#define enb = 5;
+
+int M1_Speed = 80; // la vitesse du moteur 1
+int M2_Speed = 80; // la vitesse du moteur 2
+
+char command = 0;
+
+void setup() {
+  pinMode(in1,OUTPUT);
+  pinMode(in2,OUTPUT);
+  pinMode(in3,OUTPUT);
+  pinMode(in4,OUTPUT);
+  pinMode(enA,OUTPUT);
+  pinMode(enB,OUTPUT);
+}
+
+void loop() {
+  if (command == 1){
+    forward();
+  } 
+  else if (command == 2){
+    backward();
+  }
+  else if (command == 3){
+    right();
+  }
+  else if (command == 4) {
+    right();
+  }
+}
+
+void stop() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+}
+
+void forward() {
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  analogWrite(enA, M1_Speed);
+  analogWrite(enB, M2_Speed);
+}
+
+void backward() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+  analogWrite(enA, M1_Speed);
+  analogWrite(enB, M2_Speed);
+}
+
+void left() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  analogWrite(enA, M1_Speed);
+  analogWrite(enB, M2_Speed);
+}
+
+void right() {
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  analogWrite(enA, M1_Speed);
+  analogWrite(enB, M2_Speed);
+}
